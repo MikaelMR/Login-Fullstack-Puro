2b197886cb1bb9893453d598f32807ab9cd73c53
MikaelMR <mikaramos.pro@gmail.com>
2025-09-04 18:01:58 -0300

    script fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function ativarForm (formID) {
-    document.querySelectorAll(".form-caixa")
-    document.getElementById(".form-caixa").style.display = 'block'
+    document.querySelectorAll(".form-caixa").forEach(form => form.classList.remove("active"))
+    document.getElementById(formID).classList.add("active")
 }
